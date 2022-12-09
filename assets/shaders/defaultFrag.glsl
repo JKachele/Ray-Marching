0cae7d78b0fe0672bac08065463e914eed1653e2
justinkachele <justin@kachele.com>
2022-12-09 10:03:14 -0500

    initial commit

--- assets/shaders/defaultFrag.glsl
+++ assets/shaders/defaultFrag.glsl
@@ -1,14 +1,96 @@
 #version 460 core
+#include hg_sdf.glsl
+layout (location = 0) out vec4 fragColor;
 
-in vec2 fUVCoords;
-in vec4 fColor;
+uniform vec2 uResolution;
+uniform float uTime;
 
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+const float FOV = 1.0;
+const int MAX_STEPS = 256;
+const float MAX_DIST = 500;
+const float EPSILON = 0.001;
 
-out vec4 color;
+vec2 fOpUnionID(vec2 obj1, vec2 obj2) {
+    return (obj1.x < obj2.x) ? obj1 : obj2;
+}
+
+vec2 map(vec3 ray) {
+    // Plane
+    float planeDist = fPlane(ray, vec3(0, 1, 0), 1.0);
+    float planeID = 2.0;
+    vec2 plane = vec2(planeDist, planeID);
+    // Sphere
+    float sphereDist = fSphere(ray, 1.0);
+    float sphereID = 1.0;
+    vec2 sphere = vec2(sphereDist, sphereID);
+    // Result
+    vec2 result = fOpUnionID(sphere, plane);
+    return result;
+}
+
+vec2 rayMarch(vec3 rayOrigin, vec3 rayDirection) {
+    vec2 hit;
+    vec2 object;
+    for (int i = 0; i < MAX_STEPS; i++) {
+        vec3 ray = rayOrigin + object.x * rayDirection;
+        hit = map(ray);
+        object.x += hit.x;
+        object.y = hit.y;
+        if (abs(hit.x) < EPSILON || object.x > MAX_DIST) break;
+    }
+    return object;
+}
+
+vec3 getNormal(vec3 ray) {
+    vec2 e = vec2(EPSILON, 0.0);
+    vec3 n = vec3(map(ray).x) - vec3(map(ray - e.xyy).x, map(ray - e.yxy).x, map(ray - e.yyx).x);
+    return normalize(n);
+}
+
+vec3 getLight(vec3 ray, vec3 rayDist, vec3 color) {
+    vec3 lightPos = vec3(20.0, 40.0, -30.0);
+    vec3 L = normalize(lightPos - ray);
+    vec3 N = getNormal(ray);
+
+    vec3 diffuse = color * clamp(dot(L, N), 0.0, 1.0);
+
+    // Shadows
+    float distToObj = rayMarch(ray + N * 0.02, normalize(lightPos)).x;
+    if (distToObj < length(lightPos - ray)) return vec3(0);
+    return diffuse;
+}
+
+vec3 getMaterial(vec3 ray, float id) {
+    vec3 material = vec3(1);
+    switch (int(id)) {
+        case 1:
+        material = vec3(0.9, 0.9, 0.0);break;
+        case 2:
+        material = vec3(0.2 + 0.4 * mod(floor(ray.x) + floor(ray.z), 2.0));break;
+    }
+    return material;
+}
+
+void render(inout vec3 col, in vec2 uv) {
+    vec3 rayOrigin = vec3(0, 0, -3.0);
+    vec3 rayDir = normalize(vec3(uv, FOV));
+
+    vec2 object = rayMarch(rayOrigin, rayDir);
+
+    if (object.x < MAX_DIST) {
+        vec3 ray = rayOrigin + object.x * rayDir;
+        vec3 material = getMaterial(ray, object.y);
+        col += getLight(ray, rayDir, material);
+    }
+}
 
 void main() {
-    color = texture(texture2, fUVCoords) * fColor;
-//    color = fColor;
+    vec2 uv = (2.0 * gl_FragCoord.xy - uResolution.xy) / uResolution.y;
+
+    vec3 col = vec3(0.0, 0.0, 0.0);
+    render(col, uv);
+
+    // Gamma correction
+    col = pow(col, vec3(0.4545));
+    fragColor = vec4(col, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,240 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <glfw3.h>
+#include <Shadinclude.hpp>
+#include <cstring>
+
+using std::cout;
+using std::cerr;
+using std::string;
+using std::endl;
+
+int windowWidth;
+int windowHeight;
+GLFWwindow* glfwWindow;
+unsigned int shaderProgramID;
+unsigned int vaoID;
+unsigned int vboID;
+unsigned int eboID;
+
+float vertexArray[12] {
+    -1.0f, -1.0f, 0.0f,
+    -1.0f,  1.0f, 0.0f,
+     1.0f,  1.0f, 0.0f,
+     1.0f,-1.0f,0.0f
+};
+
+int elementArray[6] = {
+        0, 2, 1,
+        0, 3, 2
+};
+
+void startWindow(const char* title) {
+    if (!glfwInit()) {
+        cerr << "Failed to initialize GLFW" << endl;
+        return;
+    }
+
+    // Set the opengl version to opengl version 4.6 core
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);   // Window will not be visible during creation
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);  // Window will be resizeable
+
+    // Create the window
+    glfwWindow = glfwCreateWindow(windowWidth, windowHeight, title, nullptr, nullptr);
+    if (glfwWindow == nullptr) {
+        cerr << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return;
+    }
+
+    glfwMakeContextCurrent(glfwWindow);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        cerr << "Failed to initialize GLAD" << endl;
+        glfwTerminate();
+        return;
+    }
+
+    // set the viewport to the size of the window
+    glViewport(0, 0, windowWidth, windowHeight);
+    glfwSetFramebufferSizeCallback(glfwWindow, [](GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+        windowWidth = width;
+        windowHeight = height;
+    });
+
+    // Set the window position to the middle of the screen
+    const GLFWvidmode* vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    if (vidMode != nullptr) {
+        glfwSetWindowPos(glfwWindow,
+                         (vidMode->width - windowWidth) / 2, (vidMode->height - windowHeight) / 2);
+    }
+
+    glfwSwapInterval(1);    // Enable v-sync: Sets max FPS to screen refresh rate
+
+    glfwShowWindow(glfwWindow);
+}
+
+void initShader(const string& vertexShaderFile, const string& fragmentShaderFile) {
+    string vertexShaderString;
+    string fragmentShaderString;
+    const char* vertexShader;
+    const char* fragmentShader;
+
+    vertexShaderString = Shadinclude::load(vertexShaderFile);
+    vertexShader = strcpy(new char[vertexShaderString.length() + 1],
+                                vertexShaderString.c_str());
+    fragmentShaderString = Shadinclude::load(fragmentShaderFile);
+    fragmentShader = strcpy(new char[fragmentShaderString.length() + 1],
+                                  fragmentShaderString.c_str());
+
+    // =========================================================
+    // Compile the shaders
+    // =========================================================
+    unsigned int vertexID;
+    unsigned int fragmentID;
+
+    // create a unique id for the vertex shader
+    vertexID = glCreateShader(GL_VERTEX_SHADER);
+
+    // Load and compile the vertex shader
+    glShaderSource(vertexID, 1, &vertexShader, nullptr);
+    glCompileShader(vertexID);
+
+    // Check for shader compile errors
+    int success;
+    glGetShaderiv(vertexID, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetShaderInfoLog(vertexID, 512, nullptr, infoLog);
+        cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
+    }
+    // create a unique id for the fragment shader
+    fragmentID = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Load and compile the fragment shader
+    glShaderSource(fragmentID, 1, &fragmentShader, nullptr);
+    glCompileShader(fragmentID);
+
+    // Check for shader compile errors
+    glGetShaderiv(fragmentID, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetShaderInfoLog(fragmentID, 512, nullptr, infoLog);
+        cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << endl;
+    }
+
+    // =========================================================
+    // link the shaders
+    // =========================================================
+    shaderProgramID = glCreateProgram();
+    glAttachShader(shaderProgramID, vertexID);
+    glAttachShader(shaderProgramID, fragmentID);
+    glLinkProgram(shaderProgramID);
+
+    // Check for linking errors
+    glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(shaderProgramID, 512, nullptr, infoLog);
+        cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << endl;
+    }
+    glDeleteShader(vertexID);
+    glDeleteShader(fragmentID);
+}
+
+void uploadFloat(const string &varName, const float val) {
+    int varLocation = glGetUniformLocation(shaderProgramID, varName.c_str());
+    glUseProgram(shaderProgramID);
+    glUniform1f(varLocation, val);
+}
+
+void uploadVec2(const string &varName, const float valX, const float valY) {
+    int varLocation = glGetUniformLocation(shaderProgramID, varName.c_str());
+    glUseProgram(shaderProgramID);
+    glUniform2f(varLocation, valX, valY);
+}
+
+void initScene() {
+    std::string defaultShaderVert = "../assets/shaders/defaultVert.glsl";
+    std::string defaultShaderFrag = "../assets/shaders/defaultFrag.glsl";
+    initShader(defaultShaderVert, defaultShaderFrag);
+
+    // ============================================================
+    // Generate VAO, VBO, and EBO buffer objects, and send to GPU
+    // ============================================================
+    glGenVertexArrays(1, &vaoID);
+    glBindVertexArray(vaoID);
+
+    glGenBuffers(1, &vboID);
+    glBindBuffer(GL_ARRAY_BUFFER, vboID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexArray), vertexArray, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &eboID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elementArray), elementArray, GL_STATIC_DRAW);
+
+    // Add the vertex Attribute pointers
+    int posSize = 3;
+    int vertexSizeBytes = posSize * sizeof(float);
+
+    glVertexAttribPointer(0, posSize, GL_FLOAT, false, vertexSizeBytes, 0);
+    glEnableVertexAttribArray(0);
+}
+
+void render() {
+    glUseProgram(shaderProgramID);
+
+    uploadVec2("uResolution", windowWidth, windowHeight);
+    uploadFloat("uTime", glfwGetTime());
+
+
+    glBindVertexArray(vaoID);
+
+    glEnableVertexAttribArray(0);
+
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glDisableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+    glUseProgram(0);
+}
+
+void gameLoop() {
+    float beginTime = glfwGetTime();
+    float endTime;
+    float dt = -1;
+
+    glClearColor(0, 0, 0, 1);     // Sets the background color of the screen
+
+    while(!glfwWindowShouldClose(glfwWindow)) {
+
+        glfwPollEvents();       // looks or events like a mouse or keyboard being pressed
+
+        glClear(GL_COLOR_BUFFER_BIT);       // Sets the color of the screen to be updated
+
+        if (dt >= 0) {
+            render();
+        }
+
+        glfwSwapBuffers(glfwWindow);   // Render the new frame to the window
+
+        endTime = glfwGetTime();        // Calculate the tine to render a frame (Framerate)
+        dt = endTime - beginTime;
+        beginTime = endTime;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    windowWidth = 1920;
+    windowHeight = 1080;
+    startWindow("RayMarching");
+    initScene();
+    gameLoop();
     return 0;
 }
